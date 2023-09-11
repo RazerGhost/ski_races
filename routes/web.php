2863e6c8789ee6eb0fb81018212b8caad6d1d2c8
RazerGhost <IRazerIGhost@gmail.com>
2023-09-11 22:07:48 +0200

    restart
    class changes etc
    
    1 step further :skull:
    
    hate this shit
    
    fixed caps letters
    
    fix

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\RaceBoardViewController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -27,8 +26,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    Route::get('/raceboard', [RaceBoardViewController::class, 'index'])->name('raceboard.index');
 });
 
 require __DIR__.'/auth.php';
